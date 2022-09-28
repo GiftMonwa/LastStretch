7e49c6af2ad8be5e3a9d67f288a868a4d2209cac
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-09-28 19:01:10 +0200

    Simple Modules for our app

--- Website/LastStretch/src/app/app.module.ts
+++ Website/LastStretch/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FinalYeaComponent } from './final-yea/final-yea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavigationBarComponent,
+    FinalYeaComponent
   ],
   imports: [
     BrowserModule,
---
+++ Website/LastStretch/src/app/final-yea/final-yea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-final-yea',
+  templateUrl: './final-yea.component.html',
+  styleUrls: ['./final-yea.component.scss']
+})
+export class FinalYeaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
