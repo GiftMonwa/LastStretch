4d59b63ae1b25abd9d5a2e46ea8d5f514eaed7c7
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-10-06 04:37:39 +0200

    ✨ Added the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,27 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { HomePageComponent } from './home-page/home-page.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { Page5Component } from './page5/page5.component';
+import { Page6Component } from './page6/page6.component';
+import { Page7Component } from './page7/page7.component';
+import { Page8Component } from './page8/page8.component';
+import { Page9Component } from './page9/page9.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    Page2Component,
+    Page3Component,
+    Page4Component,
+    Page5Component,
+    Page6Component,
+    Page7Component,
+    Page8Component,
+    Page9Component
   ],
   imports: [
     BrowserModule,
